803d15a237ce9ba460f9acc212e8285b577ddb14
erkmen uzman <user.name>
2024-11-02 22:08:41 +0300

    Added server creation on HTTP and tried using nodemon

---
+++ HTTPS_server.js
@@ -0,0 +1,13 @@
+import http from "http"
+const PORT = 8000;
+
+const server = http.createServer ((req,res) => {
+    // res.setHeader('Content-type', 'text/html');
+
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end('<h1>Hello world!</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
