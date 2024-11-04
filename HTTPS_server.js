6cdf38a40f19cb1e43ae7798d40a54880a05483f
erkmen uzman <user.name>
2024-11-04 20:27:57 +0300

    HTML structure is added, async function is added also exceptions are added for wrong requests

--- HTTPS_server.js
+++ HTTPS_server.js
@@ -1,13 +1,51 @@
-import http from "http"
+import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+
 const PORT = process.env.PORT;
 
-const server = http.createServer ((req,res) => {
-    // res.setHeader('Content-type', 'text/html');
-    console.log(req.url);
-    console.log(req.method);
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('<h1>Hello world!</h1>');
+console.log(__filename, __dirname);
+
+const server = http.createServer (async(req,res) => {
+
+    try{
+        // Check if GET request
+        if(req.method === 'GET')  {
+
+            let filePath;
+
+            if(req.url === '/')
+                {
+                    filePath = path.join(__dirname, 'public', 'index.html');
+                }   
+                
+                else if(req.url === '/about')
+                {
+                    filePath = path.join(__dirname, 'public', 'about.html');
+                }
+            
+                else
+                {
+                    throw new Error('Not Found');
+                }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-type','text/html');
+            res.write(data);
+            res.end();
+        }
+        else    {
+            throw new Error('Method not allowed');
+        }
+    }   catch(error)    {
+        res.writeHead(500, {'Content-type': 'text/plain'});
+        res.end('<h1>Server Error</h1>'); 
+    }
+    
 });
 
 server.listen(PORT, () => {
