0c854389ec06dec42da8a6fe673ce240911b26c0
erkmen uzman <user.name>
2024-11-07 19:17:38 +0300

    Created a user database which holds id and name, adjusted request method to respond different users and a no user situation

--- HTTPS_server2.js
+++ HTTPS_server2.js
@@ -13,15 +13,37 @@ const server = createServer((req, res) => {
     console.log(`Request URL: ${req.url}, Method: ${req.method}`); // Log incoming requests for debugging
 
     // Check for the exact route and method
-    if (req.url === '/api/users' && req.method === 'GET') { 
+    if (req.url === '/api/users' && req.method === 'GET') 
+    { 
         res.setHeader('Content-Type', 'application/json');
         res.writeHead(200); // Send a 200 OK status
         res.end(JSON.stringify(users)); // Send JSON data as response
-    } else {
-        // Respond with 404 for other routes
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+    } 
+
+    else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') 
+    {
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        res.setHeader('Content-Type', 'application/json');
+        if(user) 
+        {
+            res.writeHead(200); // Send a 200 OK status
+            res.end(JSON.stringify(user)); // Send JSON data as response
+        } 
+        else 
+        {
+            // Respond with 404 for other routes
+            res.write(JSON.stringify({message: 'User not found'}));            
+        }
+        res.end();
     }
+    else
+    {
+        res.setHeader('Content-type', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: 'Route not found'}));
+        res.end();
+    } 
 });
 
 server.listen(PORT, () => {
